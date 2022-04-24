ab367655cd3244b7349b9e1a1716637df7a5e987
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-23 23:18:29 -0400

    chapter 03

--- chapter01/src/main/java/com/talentyco/chapter01/domain/Router.java
+++ chapter01/src/main/java/com/talentyco/chapter01/domain/Router.java
@@ -1,6 +1,5 @@
 package com.talentyco.chapter01.domain;
 
-
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
---
+++ chapter03/src/test/java/com/talentyco/chapter03/Chapter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.chapter03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
